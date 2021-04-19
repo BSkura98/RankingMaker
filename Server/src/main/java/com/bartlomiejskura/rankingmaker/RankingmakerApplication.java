a096894810fedc0892985343f64b5f499ba4884f
215877 <bartek33s@wp.pl>
2021-04-19 20:22:20 +0200

    First commit

---
+++ Server/src/main/java/com/bartlomiejskura/rankingmaker/RankingmakerApplication.java
@@ -0,0 +1,13 @@
+package com.bartlomiejskura.rankingmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankingmakerApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/bartlomiejskura/rankingmaker/RankingmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartlomiejskura.rankingmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
