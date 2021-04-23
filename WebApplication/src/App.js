9da067e63d9914e3657b22fcd07bf7ab7e66b653
215877 <bartek33s@wp.pl>
2021-04-24 00:13:26 +0200

    [Web App] Added react router

--- WebApplication/src/App.js
+++ WebApplication/src/App.js
@@ -1,180 +1,19 @@
-import React, { useState, useEffect } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Card,
-  Container,
-  ListGroup,
-  Form,
-  Col,
-  Button,
-  Row,
-} from "react-bootstrap";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import Item from "./components/Item";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import RankingList from "./RankingList";
+import Ranking from "./Ranking";
 
-const api = axios.create({
-  baseURL: `http://localhost:8080/item`,
-});
-
-const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    setData(data);
-    setLoading(false);
-  }, []);
-
-  return { data, loading };
+const ReactRouterSetup = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <RankingList />
+        </Route>
+        <Route path="/ranking/:rankingId" children={<Ranking />}></Route>
+      </Switch>
+    </Router>
+  );
 };
 
-function App() {
-  const rankingId = 1;
-
-  const [items, setItems] = useState({});
-  const { data, loading } = useFetch(
-    "http://localhost:8080/ranking?rankingId=" + rankingId
-  );
-  const [newName, setNewName] = useState("");
-  const [newDescription, setNewDescription] = useState("");
-
-  const handleOnDragEnd = (result) => {
-    if (!result.destination) return;
-    const newItems = Array.from(items);
-    const [reorderedItem] = newItems.splice(result.source.index, 1);
-    newItems.splice(result.destination.index, 0, reorderedItem);
-    newItems.map((item, index) => {
-      item.position = index + 1;
-    });
-
-    setItems(newItems);
-  };
-
-  const addItem = (e) => {
-    e.preventDefault();
-    if (newName) {
-      const item = {
-        position: items.length + 1,
-        name: newName,
-        description: newDescription,
-        id: "falseid" + new Date().getTime().toString(),
-        ranking: { id: rankingId },
-      };
-      setItems((items) => {
-        return [...items, item];
-      });
-      setNewName("");
-      setNewDescription("");
-    }
-  };
-
-  const updateItems = async (e) => {
-    e.preventDefault();
-    let updatedItems = items;
-    updatedItems = updatedItems.map((item) => {
-      console.log(item);
-      if (item.id.startsWith("falseid")) {
-        item.id = null;
-      } else {
-        item.id = parseInt(item.id);
-      }
-      item.ranking = { id: rankingId };
-      return item;
-    });
-
-    const result = await api.put("/updateItems", updatedItems);
-    if (result.data != null) {
-      setItems(
-        result.data
-          .sort((a, b) => (a.position > b.position ? 1 : -1))
-          .map((item) => {
-            return { ...item, id: item.id.toString() };
-          })
-      );
-    }
-  };
-
-  const displayItems = (e) => {
-    e.preventDefault();
-    console.log(items);
-  };
-
-  useEffect(() => {
-    if (data != null) {
-      setItems(
-        data.items
-          .sort((a, b) => (a.position > b.position ? 1 : -1))
-          .map((item) => {
-            return { ...item, id: item.id.toString() };
-          })
-      );
-    }
-  }, [data]);
-
-  return (
-    <div className="App">
-      <Container>
-        <Card style={{ marginTop: "5rem" }}>
-          <h1 className="text-center">Ranking</h1>
-          <Row>
-            <Col>
-              <Button onClick={(e) => displayItems(e)}>items</Button>
-            </Col>
-            <Col>
-              <Button onClick={(e) => updateItems(e)}>save</Button>
-            </Col>
-          </Row>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <div>
-              <Form className="mb-2 ml-2 mr-2" onSubmit={addItem}>
-                <div className="form-row">
-                  <Col>
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="name"
-                      value={newName}
-                      onChange={(e) => setNewName(e.target.value)}
-                    />
-                  </Col>
-                  <Col className="col-9">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="description"
-                      value={newDescription}
-                      onChange={(e) => setNewDescription(e.target.value)}
-                    />
-                  </Col>
-                  <Button type="submit">Add</Button>
-                </div>
-              </Form>
-              <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId="items">
-                  {(provided) => (
-                    <ListGroup
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                    >
-                      {items.map((item, index) => {
-                        return <Item item={item} index={index} />;
-                      })}
-                    </ListGroup>
-                  )}
-                </Droppable>
-              </DragDropContext>
-            </div>
-          )}
-        </Card>
-      </Container>
-    </div>
-  );
-}
-
-export default App;
+export default ReactRouterSetup;
